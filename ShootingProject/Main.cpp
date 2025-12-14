aa218677e9f17ba87e96068279a6d45b46bf4978
nahn945 <yukikobayashie97@gmail.com>
2025-12-14 20:17:13 +0900

    feat: ゲームの骨組み作成
    ゲーム開始後、Titleシーンでエンターキーを押すとGameシーンに遷移するプログラムを作成

---
+++ ShootingProject/Main.cpp
@@ -0,0 +1,30 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.16
+# include "./Scenes/Scene.h"
+
+
+void Main()
+{
+	App manager;
+	manager.add<Title>(U"Title");
+	manager.add<Game>(U"Game");
+
+	manager.init(U"Title");
+
+	while (System::Update())
+	{
+		if (!manager.update())
+		{
+			break;
+		}
+	}
+}
+
+//
+// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
+//
+// - Release ビルド: 最大限の最適化でビルドします。
+//
+// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
+//
+// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要な場合があります。
+//
---
+++ ShootingProject/Scenes/Game.cpp
@@ -0,0 +1,17 @@
+﻿# include "./Scene.h"
+
+Game::Game(const InitData& init)
+	: IScene{ init }
+{
+
+}
+
+void Game::update()
+{
+	Print << U"This is the Game scene";
+}
+
+void Game::draw() const
+{
+
+}
