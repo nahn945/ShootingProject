15ee74db953677123773f3c737f783546d348d26
nahn945 <yukikobayashie97@gmail.com>
2025-12-15 19:07:56 +0900

    a

--- ShootingProject/Scenes/Game.cpp
+++ ShootingProject/Scenes/Game.cpp
@@ -3,15 +3,16 @@
 Game::Game(const InitData& init)
 	: IScene{ init }
 {
-
+	player.setConfig(defaultConfig);
 }
 
 void Game::update()
 {
-	Print << U"This is the Game scene";
+	//Print << U"This is the Game scene";
+	player.update();
 }
 
 void Game::draw() const
 {
-
+	player.draw();
 }
